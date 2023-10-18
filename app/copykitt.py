debc85f10451b69f673c344d5667cdea5f47ca60
iamdhrutipatel <dhrutipatel0100@gmail.com>
2023-10-18 12:23:21 -0700

    SaaS App

--- app/copykitt.py
+++ app/copykitt.py
@@ -4,5 +4,12 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
-print(chat_completion)
+subject = "coffee"
+prompt = f"Generate upbeat branding snippet for {subject}"
+response = openai.Completion.create(
+  model="gpt-3.5-turbo-instruct",
+  prompt=prompt,
+  max_tokens=32
+)
+
+print(response)
